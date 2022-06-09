3284918058abf75d7aae323b1186deb3056e42a4
“bai21018” <bai21018@byui.edu>
2022-06-09 14:40:22 -0600

    Finished!! It loops around each time too!

--- tictactoe_MattBailey.py
+++ tictactoe_MattBailey.py
@@ -1,60 +1,72 @@
 # Matthew Bailey, Tic Tac Toe Assignment
+import time 
 
+play_loop = True
 
 def main():
-    player = next_player("")
-    board = create_board()
-    while not (has_winner(board) or is_a_draw(board)):
-        # display/create the board
-        display_board(board)
-        # allow the user to 'move'
-        make_move(player, board)
-        #toggle which player makes which move on the board
-        player = next_player(player)
-    #re-display the board after each move has been made
-    display_board(board)
-    print("Good game. Thanks for playing!") 
+    while play_loop == True:
+        print("Hello and welcome to a simple version of Tic Tac Toe! Would you like to play?")
+        play_again = input("Type 'y' for yes and 'n' for no: ")
+        if play_again.lower() == 'y':
+            print("Generating player board...")
+            time.sleep(5)
+            player = next_player("")
+            block = create_block()
+            while not (has_winner(block) or is_a_draw(block)):
+                # display/create the block
+                display_block(block)
+                # allow the user to 'move'
+                make_move(player, block)
+                #toggle which player makes which move on the block
+                player = next_player(player)
+            #re-display the block after each move has been made
+            display_block(block)
+            print("Game Over. Try again?") 
+        else:
+            print("Thank you for playing!")
+        play_again = input("Type 'y' for yes and 'n' for no: ")
+    
 
-def create_board():
-    #create the board by using an array/dictonary, starting with an empy dictonrary 
-    board = []
+def create_block():
+    #create the block by using an array/dictonary, starting with an empy dictonrary 
+    block = []
     for square in range(9):
         #iterate through the dictronary, square is a 'tempoarary' variable
-        board.append(square + 1)
+        block.append(square + 1)
         #this is where we label each of the 9 squares. Remember, starts with 0 
-    return board
+    return block
 
-def display_board(board):
-    #create the board the user will be able to see (this is provided!)
+def display_block(block):
+    #create the block the user will be able to see (this is provided!)
     print()
-    print(f"{board[0]} | {board[1]} | {board[2]}")
+    print(f"{block[0]} | {block[1]} | {block[2]}")
     print('- + - + -')
-    print(f"{board[3]} | {board[4]} | {board[5]}")
+    print(f"{block[3]} | {block[4]} | {block[5]}")
     print('- + - + -')
-    print(f"{board[6]} | {board[7]} | {board[8]}")
+    print(f"{block[6]} | {block[7]} | {block[8]}")
     print()
     
-def is_a_draw(board):
-    #This is saying if the isn't a winner, the board will declare it a tie
+def is_a_draw(block):
+    #This is saying if the isn't a winner, the block will declare it a tie
     for square in range(9):
-        if board[square] != "X" and board[square] != "O":
+        if block[square] != "X" and block[square] != "O":
             return False
     print("It's a tie!")
     return True 
     
-def has_winner(board):
-    return (board[0] == board[1] == board[2] or
-            board[3] == board[4] == board[5] or
-            board[6] == board[7] == board[8] or
-            board[0] == board[3] == board[6] or
-            board[1] == board[4] == board[7] or
-            board[2] == board[5] == board[8] or
-            board[0] == board[4] == board[8] or
-            board[2] == board[4] == board[6])
+def has_winner(block):
+    return (block[0] == block[1] == block[2] or
+            block[3] == block[4] == block[5] or
+            block[6] == block[7] == block[8] or
+            block[0] == block[3] == block[6] or
+            block[1] == block[4] == block[7] or
+            block[2] == block[5] == block[8] or
+            block[0] == block[4] == block[8] or
+            block[2] == block[4] == block[6])
 
-def make_move(player, board):
+def make_move(player, block):
     square = int(input(f"{player}'s turn to choose a square (1-9): "))
-    board[square - 1] = player
+    block[square - 1] = player
     if square != 1 or 2 or 3 or 4 or 5 or 6 or 7 or 8 or 9:
         print("Please try again. Only the numbers 1-9 are accepted.")
 
